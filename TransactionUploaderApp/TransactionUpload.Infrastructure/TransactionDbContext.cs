350b669e8f34700ce3c80e08539b9361a3b6bd8c
Mia Mahmudunnabi <mia.mahmudunnabi@petronas.com>
2024-10-05 11:41:59 +0800

    Create initial solution >API,Application,Infra & domain & added model & dbContext Class

---
+++ TransactionUploaderApp/TransactionUpload.Infrastructure/TransactionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace TransactionUpload.Infrastructure
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ TransactionUploaderApp/TrasanctionUpload.Domain/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrasanctionUpload.Domain.Models
+{
+    public class Transaction
+    {
+        public string TransactionId { get; set; }
+        public string AccountNo { get; set; }
+        public decimal Amount { get; set; }
+        public string CurrencyCode { get; set; }
+        public string Status { get; set; }
+
+    }
+}
